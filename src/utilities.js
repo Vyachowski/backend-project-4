ccc500e39e34b68880f1bee729e80cd2a9397fe9
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2024-03-29 15:28:47 +0300

    refactor: add separate utility files

---
+++ src/utilities.js
@@ -0,0 +1,29 @@
+import path from 'path';
+
+const isValidUrl = (urlString) => {
+  const urlPattern = new RegExp('^(https?:\\/\\/)?'
+        + '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'
+        + '((\\d{1,3}\\.){3}\\d{1,3}))' //
+        + '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'
+        + '(\\?[;&a-z\\d%_.~+=-]*)?'
+        + '(\\#[-a-z\\d_]*)?$', 'i');
+  return !!urlPattern.test(urlString);
+};
+
+const generateFileNameFromUrl = (url) => {
+  if (typeof url !== 'string') {
+    throw new Error('Not a valid url string');
+  }
+
+  const urlWithoutProtocol = url.replace(/^(https?:\/\/)?/, '');
+  const symbolsReplacedUrl = urlWithoutProtocol.replace(/[^a-zA-Z0-9]/g, '-');
+  return `${symbolsReplacedUrl}.html`;
+};
+
+const generateFilePath = (dirPath, filePath) => path.join(dirPath, filePath);
+
+export {
+  isValidUrl,
+  generateFileNameFromUrl,
+  generateFilePath,
+}
