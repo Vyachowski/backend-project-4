a0bdbf40d960f0e684111c1ceb1872751dc32e03
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2024-04-26 23:08:40 +0300

    refactor: add localhost as a valid address

--- src/utilities.js
+++ src/utilities.js
@@ -2,8 +2,10 @@ import path from 'path';
 
 const isValidUrl = (urlString) => {
   const urlPattern = new RegExp('^(https?:\\/\\/)?'
+        + '(localhost|'
         + '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'
-        + '((\\d{1,3}\\.){3}\\d{1,3}))' //
+        + '((\\d{1,3}\\.){3}\\d{1,3}))'
+        + ')'
         + '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'
         + '(\\?[;&a-z\\d%_.~+=-]*)?'
         + '(\\#[-a-z\\d_]*)?$', 'i');
